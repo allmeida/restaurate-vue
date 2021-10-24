bd9d2fb8e54a9e1cd31fd9c2936b8779e02306c1
allmeida <https://github.com/allmeida>
2021-10-24 14:05:31 -0400

    Listando items com axios

---
+++ src/components/Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="item">
+      <h2>{{item.name}}</h2>
+      <p>{{item.description}}</p>
+      <p>{{item.price}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Item',
+    props: {
+        item: {}
+    }
+}
+</script>
+
+<style>
+
+</style>
